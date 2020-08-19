7cb3196491005183fcf9644a8f345255d09d3fd7
wrakc <carlosveiga1618@gmail.com>
2020-08-19 14:19:44 -0300

    add main files using bundle install

---
+++ lib/office/workout.rb
@@ -0,0 +1,8 @@
+require "office/workout/version"
+
+module Office
+  module Workout
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/office/workout/version.rb
@@ -0,0 +1,5 @@
+module Office
+  module Workout
+    VERSION = "0.1.0"
+  end
+end
