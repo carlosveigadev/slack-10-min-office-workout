779e69251b70ccbf682faef38d400a1f11b14573
wrakc <carlosveiga1618@gmail.com>
2020-08-20 12:18:56 -0300

    add workouts.rb proper module for the arrays with the workouts.

--- lib/bot.rb
+++ lib/bot.rb
@@ -7,7 +7,14 @@ module OfficeWorkout
       command :get_workout do
         title 'get_workout'
         desc 'Returns a random link of YouTube workout that can be done at your office.'
-        long_desc 'The competitiveness of professionals in the labor market, the fierce competition of companies, the pursuit for increased productivity and the increasing demands of consumers form the perfect set to expose individuals to high levels of tension and stress, compromising the quality of life and the health of the workers. Therefore, programs designed for promoting health and improving the quality of life of employees have become indispensable and must integrate the culture of companies. This bot helps office workers and people who are working at home, to reserve 10 minutos of their day and follow the random tutorial in order to achieve quality of life.'
+        long_desc 'The competitiveness of professionals in the labor market, the fierce competition of companies, 
+        the pursuit for increased productivity and the increasing demands of consumers form the perfect 
+        set to expose individuals to high levels of tension and stress, compromising the quality of 
+        life and the health of the workers. Therefore, programs designed for promoting health and 
+        improving the quality of life of employees have become indispensable and must integrate 
+        the culture of companies. This bot helps office workers and people who are working at home, 
+        to reserve 10 minutos of their day and follow the random tutorial in order to achieve quality 
+        of life.'
       end
     end
   end
--- lib/commands/get_workout.rb
+++ lib/commands/get_workout.rb
@@ -1,13 +1,11 @@
-require_relative 'lib/workouts/workouts.rb'
+require './lib/workouts/workouts.rb'
 
 module OfficeWorkout
   module Commands
     class GetWorkout < SlackRubyBot::Commands::Base
       command 'get_workout' do |client, data, _match|
-        client.say(channel: data.channel, text: Workouts.)
+        client.say(channel: data.channel, text: Workouts.workouts_list[rand(0...Workouts.workouts_list.length)])
       end
     end
   end
 end
-require './lib/workouts/workouts.rb'
-puts workouts[rand(0...workouts.length)]
