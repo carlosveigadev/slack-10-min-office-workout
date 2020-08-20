78d141827f4e5bf81647f6f0d5d69f4ab32e9a43
wrakc <carlosveiga1618@gmail.com>
2020-08-20 12:06:31 -0300

    fix workouts name to proper name

--- lib/commands/get_workout.rb
+++ lib/commands/get_workout.rb
@@ -1,11 +1,13 @@
-# module OfficeWorkout
-#   module Commands
-#     class GetWorkout < SlackRubyBot::Commands::Base
-#       command 'get_workout' do |client, data, _match|
-#         client.say(channel: data.channel, text: 'this is the text')
-#       end
-#     end
-#   end
-# end
+require_relative 'lib/workouts/workouts.rb'
+
+module OfficeWorkout
+  module Commands
+    class GetWorkout < SlackRubyBot::Commands::Base
+      command 'get_workout' do |client, data, _match|
+        client.say(channel: data.channel, text: Workouts.)
+      end
+    end
+  end
+end
 require './lib/workouts/workouts.rb'
 puts workouts[rand(0...workouts.length)]
