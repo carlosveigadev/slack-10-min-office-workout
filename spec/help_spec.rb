088d70adcacd799995f340c811659f2621686d24
wrakc <carlosveiga1618@gmail.com>
2020-08-21 14:32:14 -0300

    fix rubocop offenses

--- spec/help_spec.rb
+++ spec/help_spec.rb
@@ -3,6 +3,6 @@ require './lib/commands/get_motivation.rb'
 
 describe SlackRubyBot::Commands do
   it 'responds with help message' do
-    expect(message: "#{SlackRubyBot.config.user} help").to respond_with_slack_message
+    expect(message:"#{SlackRubyBot.config.user} help").to respond_with_slack_message
   end
 end
--- spec/hi_spec.rb
+++ spec/hi_spec.rb
@@ -3,6 +3,6 @@ require './lib/commands/get_motivation.rb'
 
 describe SlackRubyBot::Commands do
   it 'responds with any message' do
-    expect(message: "#{SlackRubyBot.config.user} hi").to respond_with_slack_message
+    expect(message:"#{SlackRubyBot.config.user} hi").to respond_with_slack_message
   end
 end
