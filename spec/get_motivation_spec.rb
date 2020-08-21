cf4621afb7ca09c3363636ff951e0a6497fc38d4
wrakc <carlosveiga1618@gmail.com>
2020-08-21 13:45:14 -0300

    create a file of rspec to hi respond message

--- spec/get_motivation_spec.rb
+++ spec/get_motivation_spec.rb
@@ -2,13 +2,6 @@
 require 'slack-ruby-bot/rspec'
 require './lib/commands/get_motivation.rb'
 
-
-describe SlackRubyBot::Commands do
-  it 'responds with any message' do
-    expect(message: "#{SlackRubyBot.config.user} hi").to respond_with_slack_message
-  end
-end
-
 describe OfficeWorkout::Commands::GetMotivation do
   def app
     OfficeWorkout::Bot.instance
@@ -18,24 +11,3 @@ describe OfficeWorkout::Commands::GetMotivation do
     expect(message:  "#{SlackRubyBot.config.user} get_motivation").to be_a Hash
   end
 end
-
-
-# describe SlackStackbot::Commands::Search do
-
-#   let(:some_results) do
-#     StackOverflow::Search.new.questions('HTML api')
-#   end
-#   let(:no_results) do
-#     StackOverflow::Search.new.questions('sakdjaskldjhsdawe3')
-#   end
-#   it 'StackOverflow search returns some results' do
-#     expect(message: 'stackbot search HTML api').to respond_with_slack_message(some_results)
-#   end
-#   it 'StackOverflow search returns no results' do
-#     expect(message: 'stackbot search sakdjaskldjhsdawe3').to respond_with_slack_message(no_results)
-#   end
-#   it 'Search terms missing' do
-#     expect(message: 'stackbot search').to respond_with_slack_message('Search for what?')
-#   end
-# end
-
