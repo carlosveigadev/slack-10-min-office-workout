e772057a95d96537b90172887b608385c3113170
wrakc <carlosveiga1618@gmail.com>
2020-08-25 13:07:32 -0300

    refactor get_motivation.rb and get_workout.rb

--- lib/commands/get_motivation.rb
+++ lib/commands/get_motivation.rb
@@ -1,12 +1,12 @@
 require_relative '../data/motivational_quotes.rb'
-
 module OfficeWorkout
   module Commands
     # Class to define the get_motivation command that returns a motivational quote.
     class GetMotivation < SlackRubyBot::Commands::Base
+      motivation = Motivation.motivational_quotes
       command 'get_motivation' do |client, data, _match|
         client.say(channel: data.channel, text: "Come on, check this out:
-          #{Motivation.motivational_quotes[rand(0...Motivation.motivational_quotes.length)]}")
+          #{motivation[rand(0...motivation.length)]}")
       end
     end
   end
--- lib/commands/get_workout.rb
+++ lib/commands/get_workout.rb
@@ -4,10 +4,10 @@ module OfficeWorkout
   module Commands
     # Class to define the get_workout command that returns a YoutTube link to a workout.
     class GetWorkout < SlackRubyBot::Commands::Base
+      workout = Workouts.workouts_list
       command 'get_workout' do |client, data, _match|
-        client.say(channel: data.channel, text: "I am glad you are here! Enjoy the workout! #{Workouts.workouts_list[
-          rand(0...Workouts.workouts_list.length)
-          ]}")
+        client.say(channel: data.channel, text: "I am glad you are here! Enjoy the workout!
+          #{workout[rand(0...workout.length)]}")
       end
     end
   end
